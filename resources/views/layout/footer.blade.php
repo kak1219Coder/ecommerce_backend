12916dc42273440c8198c63a34f9a28658df7443
Kader Kone <kader.kone@aikinggroup.tech>
2024-07-24 02:43:10 +0200

    decomposition de layout

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+  <!-- Main Footer -->
+  <footer class="main-footer" style="background: rgb(174, 125, 35); color: #FFFFFF;">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.2.0
+            </div>
+            <strong>Tous droits réservés par  KAK1219 2024 &copy; {{ date('Y') }}</strong>
+   </footer>
